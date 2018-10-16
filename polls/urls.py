352c1af3ce94a927990f9210ed462b48224b0637
cactusboy <13159098@qq.com>
2018-10-16 16:20:40 +0800

    4end

--- polls/urls.py
+++ polls/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'polls'  # 加上命名空间目的是区别其他的app中的重名项目
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:pk>/vote/', views.vote, name='vote'),
 ]
